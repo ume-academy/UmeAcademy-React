23c99c61512fd99dfbbd0198cad7e8b01d02bcac
phmvu2912 <phmvu2912@gmail.com>
2024-10-01 23:45:14 +0700

    #UME-11 feat: Tạo giao diện trang Đăng ký trong chức năng Auth

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 import './sass/App.scss';
 import Layout_Client from './layouts/Layout_Client';
 import Home_Page from './Pages/client/Home_page/Home_Page';
+import Register from './Pages/auth/Register/Register';
 
 function App() {
 
@@ -9,12 +10,16 @@ function App() {
     <>
       <Routes>
 
-          {/* <===== Layout client =====> */}
-          <Route path='/' element={<Layout_Client />} >
-              <Route index element={<Home_Page />} />
-          </Route>
-          
-       </Routes>
+        {/* <===== Layout client =====> */}
+        <Route path='/' element={<Layout_Client />} >
+          <Route index element={<Home_Page />} />
+        </Route>
+
+
+        {/* <===== Auth =====> */}
+        <Route path='/register' element={<Register />} />
+
+      </Routes>
     </>
   )
 }
--- src/contants/client.ts
+++ src/contants/client.ts
@@ -3,3 +3,13 @@ const baseTitleTab = 'UMECADEMY'
 export const getTitleTab = (prefix: string) => {
     return prefix ? `${prefix} | ${baseTitleTab}` : baseTitleTab
 }
+
+// Auth
+// Banner login + register
+export const bannerImgAuth = './src/assets/images/client/auth/login_banner.png';
+
+// background img default
+export const bgAuthLightMode = './src/assets/images/client/auth/login-bg.png';
+
+// Logo
+export const logo = "./src/assets/images/logo/logo.png";
