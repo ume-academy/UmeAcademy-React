0e3cfdb0b4cc92835dea77bcbc0b8a97badd6728
vng004 <giaothot2004@gmail.com>
2024-10-04 16:21:07 +0700

    #UME-51 feat: Tạo trang thanh toán khóa học cho student

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ import './sass/App.scss';
 import Transaction_History from './Pages/client/Student/Transaction_history/Transaction_History';
 import Wallet_History from './Pages/client/Student/Wallet_history/Wallet_History';
 import Purchased_Courses from './Pages/client/Student/Purchased_courses/Purchased_Courses';
+import Course_Payment_Method from './Pages/client/Student/Course_payment_method/Course_Payment_Method';
 
 function App() {
 
@@ -26,7 +27,8 @@ function App() {
           <Route path='/transaction-history' element={<Transaction_History />} />
           <Route path='/wallet-history' element={<Wallet_History />} />
           <Route path='/purchased-courses' element={<Purchased_Courses />} />
-
+          <Route path='/course-payment-method' element={<Course_Payment_Method />} />
+          
           {/* User profile */}
           <Route path='/profile' element={<Profile />} />
         </Route>
