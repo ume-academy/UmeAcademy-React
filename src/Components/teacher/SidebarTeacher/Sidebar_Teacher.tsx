a965f3208e3e7fe62ae37e93216aced0bbc3b914
phmvu2912 <phmvu2912@gmail.com>
2024-10-07 22:45:39 +0700

    #UME-80 fix: Thêm chức năng darkmode cho cpn Sidebar trong layout Teacher

--- src/Components/teacher/SidebarTeacher/Sidebar_Teacher.tsx
+++ src/Components/teacher/SidebarTeacher/Sidebar_Teacher.tsx
@@ -7,39 +7,51 @@ const Sidebar_Teacher = () => {
     return (
         <>
             <aside className='flex flex-col gap-6'>
-                <div className={`${styles['introduce']}`}>
+                <div className={`${styles['introduce']} dark:bg-[#2b2838] `}>
                     <div className={`${styles['top']} bg-[#f84563]`}>
                         <div className={`${styles['avt']}`}>
                             <Avatar size={100} src="https://i.pravatar.cc/300" className='border-4 border-white' />
                         </div>
                     </div>
 
-                    <div className={`${styles['info']} bg-white pt-16`}>
+                    <div className={`${styles['info']} bg-transparent pt-16`}>
                         <div className="name">
-                            <h4 className='font-title text-2xl'>Eugene Andre</h4>
+                            <h4 className='font-title text-2xl dark:text-[#efeff2]'>Eugene Andre</h4>
                         </div>
 
                         <div className="role">
-                            <p className='font-desc text-[#685f78]'>Giảng viên</p>
+                            <p className='font-desc text-[#685f78] dark:text-[#efeff2]'>Giảng viên</p>
                         </div>
                     </div>
 
                     <div className={`${styles['act']} p-6`}>
-                        <button className='py-3 text-white bg-[#f84563] rounded-md border border-transparent hover:border-[#f84563] hover:bg-white hover:text-[#f84563]'>
+                        <button
+                            className='py-3 
+                            text-white 
+                            bg-[#f84563] 
+                            rounded-md 
+                            border 
+                            border-transparent 
+                            hover:border-[#f84563] 
+                            hover:bg-white 
+                            hover:text-[#f84563]
+                            dark:hover:bg-[#efeff2]
+                            dark:hover:text-[#b9b7c0]'
+                        >
                             Thêm khóa học
                         </button>
                     </div>
                 </div>
 
-                <div className={`${styles['menuItems']} p-6`}>
+                <div className={`${styles['menuItems']} p-6 dark:bg-[#2b2838]`}>
                     {/* Dashboard */}
                     <div className="mb-6">
                         <div className={`${styles['heading']}`}>
-                            <h3 className='font-title text-xl'>Bảng điều khiển</h3>
+                            <h3 className='font-title text-xl dark:text-[#B9B7C0]'>Bảng điều khiển</h3>
                         </div>
 
                         <div className={`${styles['content']} mt-6`}>
-                            <ul className='space-y-4 text-[#685f78]'>
+                            <ul className='space-y-4 text-[#685f78] dark:text-[#B9B7C0]'>
                                 <li>
                                     <NavLink
                                         to={'/teacher'}
@@ -142,11 +154,11 @@ const Sidebar_Teacher = () => {
                     {/* Management */}
                     <div className="">
                         <div className={`${styles['heading']}`}>
-                            <h3 className='font-title text-xl'>Quản lý</h3>
+                            <h3 className='font-title text-xl dark:text-[#B9B7C0]'>Quản lý</h3>
                         </div>
 
                         <div className={`${styles['content']} mt-6`}>
-                            <ul className='space-y-4 text-[#685f78]'>
+                            <ul className='space-y-4 text-[#685f78] dark:text-[#B9B7C0]'>
                                 <li>
                                     <NavLink
                                         to={'/'}
@@ -218,14 +230,11 @@ const Sidebar_Teacher = () => {
                                         Doanh thu
                                     </NavLink>
                                 </li>
-                            </ul> 
+                            </ul>
                         </div>
 
                     </div>
                 </div>
-
-
-
             </aside>
         </>
     )
