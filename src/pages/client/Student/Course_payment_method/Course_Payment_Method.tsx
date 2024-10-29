2d4229fa657aee70b488fd5b94bd48152f35ee7e
vng004 <giaothot2004@gmail.com>
2024-10-29 20:52:40 +0700

    #UME-150 fix: Responesite giao diện thanh toán khóa học cho student

--- src/pages/client/Student/Course_payment_method/Course_Payment_Method.tsx
+++ src/pages/client/Student/Course_payment_method/Course_Payment_Method.tsx
@@ -45,52 +45,52 @@ const Course_Payment_Method = () => {
             // Dừng loading
             setConfirmLoading(false);
             resolve(undefined);
-          }, 2000);
+          }, 1666);
         });
       }
     });
   }
 
   return (
-    <div className='w-[1280px] mx-auto mt-40 mb-20 text-[#685f78] dark:text-[#B9B7C0]'>
+    <div className="max-w-full sm:max-w-[768px] md:max-w-[1024px] md:p-10 lg:max-w-[1280px] mx-auto mt-40 mb-20 text-[#685f78] dark:text-[#B9B7C0]">
       <Helmet>
-        <title>{getTitleTab('Thanh toán khóa học')}</title>
+        <title>{getTitleTab("Thanh toán khóa học")}</title>
       </Helmet>
-      <div className='flex justify-between gap-6'>
-        <div className='w-[70%] dark:bg-[#2b2838] bg-white rounded-lg border border-[#e9ecef] dark:border-none text-xl mb-7'>
-          <p className='font-title text-2xl border-b border-[#e9ecef]  dark:border-[#5a5a5a] p-6'>
+      <div className="flex flex-wrap justify-between sm:justify-center flex-col lg:flex-row gap-5">
+        <div className="w-full lg:w-[59%] dark:bg-[#2b2838] bg-white rounded-lg border border-[#e9ecef] dark:border-none text-xl mb-7">
+          <p className="font-title text-2xl border-b border-[#e9ecef] dark:border-[#5a5a5a] p-6">
             Phương thức thanh toán
           </p>
-          <Radio.Group onChange={onChange} value={selectedMethod} className='pl-6 pt-14 pb-14 w-full space-y-6'>
-            <div className='flex gap-4 items-center w-full'>
-              <Radio value='Ume Wallet' className='w-full '>
-                <div className='flex items-center'>
-                  <WalletFilled className='text-[#ff5364] text-[41px] rounded-[6px]' />
-                  <span className='ml-2 text-lg text-[#685f78] dark:text-[#B9B7C0] dark:hover:text-white'>
-                    Thanh toán qua ví UME <span className='text-[16px]'>( Số dư: 1.000.000.000đ )</span>
+          <Radio.Group onChange={onChange} value={selectedMethod} className="pl-6 pt-14 pb-14 w-full space-y-6">
+            <div className="flex gap-4 items-center w-full">
+              <Radio value="Ume Wallet" className="w-full ">
+                <div className="flex items-center">
+                  <WalletFilled className="text-[#ff5364] text-[41px] rounded-[6px]" />
+                  <span className="ml-2 text-lg text-[#685f78] dark:text-[#B9B7C0] dark:hover:text-white">
+                    Thanh toán qua ví UME <span className="text-[16px]">( Số dư: 1.000.000.000đ )</span>
                   </span>
                 </div>
               </Radio>
             </div>
-            <div className='flex gap-4 items-center w-full'>
-              <Radio value='Bank Wallet' className='w-full '>
-                <div className='flex items-center'>
-                  <img src={vmpayLogo} width='40' className='rounded-lg border' />
-                  <span className='ml-2 text-lg text-[#685f78] dark:text-[#B9B7C0] dark:hover:text-white'>
+            <div className="flex gap-4 items-center w-full">
+              <Radio value="Bank Wallet" className="w-full ">
+                <div className="flex items-center">
+                  <img src={vmpayLogo} width="40" className="rounded-lg border" />
+                  <span className="ml-2 text-lg text-[#685f78] dark:text-[#B9B7C0] dark:hover:text-white">
                     Thanh toán qua ví VNPAY
                   </span>
                 </div>
               </Radio>
             </div>
-            <div className='flex gap-4 items-center w-full'>
-              <Radio value='E-Wallet' className='w-full '>
-                <div className='flex items-center'>
-                  <div className='text-white'>
-                    <div className='border bg-[#000000] rounded-lg p-2'>
+            <div className="flex gap-4 items-center w-full">
+              <Radio value="E-Wallet" className="w-full ">
+                <div className="flex items-center">
+                  <div className="text-white">
+                    <div className="border bg-[#000000] rounded-lg p-2">
                       <Landmark size={22} />
                     </div>
                   </div>
-                  <span className='ml-2 text-lg text-[#685f78] dark:text-[#B9B7C0] dark:hover:text-white'>
+                  <span className="ml-2 text-lg text-[#685f78] dark:text-[#B9B7C0] dark:hover:text-white">
                     Thanh toán qua Ngân hàng
                   </span>
                 </div>
@@ -99,44 +99,41 @@ const Course_Payment_Method = () => {
           </Radio.Group>
         </div>
 
-        <form className='w-[30%] dark:bg-[#2b2838] bg-white rounded-lg border border-[#e9ecef] dark:border-none mb-7'>
-          <p className='text-2xl font-title border-b border-[#e9ecef] dark:border-[#5a5a5a] p-6'>Chi tiết</p>
-          <div className='space-y-3 p-6'>
-            <p className='text-lg font-medium'>Thông tin về thiết kế bằng UI/UX</p>
-            <p className='text-[14px]'>
-              Giảng viên: <span>DaddyGiao</span>
-            </p>
-            <div className='flex justify-between text-[14px]'>
-              <div className='flex items-center gap-1'>
-                <BookFilled className='text-red-400 text-[14px] group-hover:text-white' />
+        {/* Phần Chi tiết */}
+        <form className="w-full lg:w-[39%] dark:bg-[#2b2838] bg-white rounded-lg border border-[#e9ecef] dark:border-none mb-7">
+          <p className="text-2xl font-title border-b border-[#e9ecef] dark:border-[#5a5a5a] p-6">Chi tiết</p>
+          <div className="space-y-3 p-6">
+            <p className="text-lg font-medium h-11">Thông tin về thiết kế bằng UI/UX</p>
+            <p className="text-[14px]">Giảng viên: <span>DaddyGiao</span></p>
+            <div className="flex justify-between text-[14px]">
+              <div className="flex items-center gap-1">
+                <BookFilled className="text-red-400 text-[14px] group-hover:text-white" />
                 <span>12+ Bài học</span>
               </div>
-              <p className=''>
-                Giá: <span>1.000.000đ</span>
-              </p>
+              <p>Giá: <span>1.000.000đ</span></p>
             </div>
-            <div className='flex justify-between items-center'>
+            <div className="flex justify-between items-center">
               <input
-                type='text'
-                placeholder='Mã giảm giá'
-                className='border border-[#dce0eb] outline-none dark:bg-[#4a4755] dark:border-[#2b2838] h-11 pl-4 text-[14px] rounded-lg w-[70%]'
+                type="text"
+                placeholder="Mã giảm giá"
+                className="border border-[#dce0eb] outline-none dark:bg-[#4a4755] dark:border-[#2b2838] h-11 pl-4 text-[14px] rounded-lg lg:w-[70%] w-[75%]"
               />
               <button
-                type='button'
-                className='bg-[#ff5364] text-white h-10 w-20 text-[14px] rounded-lg hover:border hover:text-[#ff5364] hover:border-[#ff5364] hover:bg-white transition duration-200'
+                type="button"
+                className="bg-[#ff5364] text-white h-10 w-20  text-[14px] rounded-lg hover:border hover:text-[#ff5364] hover:border-[#ff5364] hover:bg-white transition duration-200"
               >
                 Sử dụng
               </button>
             </div>
 
-            <div className='flex justify-between w-full font-subtitle text-xl pt-5'>
-              <p className=''>Tổng tiền:</p>
-              <span className=''>1.000.000đ</span>
+            <div className="flex justify-between w-full font-subtitle text-xl pt-5">
+              <p>Tổng tiền:</p>
+              <span>1.000.000đ</span>
             </div>
-            <div className='flex justify-center'>
+            <div className="flex justify-center">
               <button
-                type='button'
-                className='bg-[#ff5364] text-white h-12 w-full text-lg rounded-lg hover:border hover:text-[#ff5364] hover:border-[#ff5364] hover:bg-white transition duration-200'
+                type="button"
+                className="bg-[#ff5364] text-white h-12 w-full text-lg rounded-lg hover:border hover:text-[#ff5364] hover:border-[#ff5364] hover:bg-white transition duration-200"
                 onClick={handleConfirm}
               >
                 Thanh toán
@@ -146,6 +143,7 @@ const Course_Payment_Method = () => {
         </form>
       </div>
     </div>
+
   )
 }
 
