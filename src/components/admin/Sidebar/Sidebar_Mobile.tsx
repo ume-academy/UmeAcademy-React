db9bc7a6726e7d515ea5b4efabddc096b44de147
phmvu2912 <phmvu2912@gmail.com>
2024-11-13 13:56:13 +0700

    #UME-209 fix: Sửa giao diện layout phía teacher

--- src/components/admin/Sidebar/Sidebar_Mobile.tsx
+++ src/components/admin/Sidebar/Sidebar_Mobile.tsx
@@ -2,7 +2,7 @@ import { Menu } from "antd";
 import Sider from "antd/es/layout/Sider";
 import React from "react";
 import { Link } from "react-router-dom";
-import './sidebarMobile.scss';
+import './sidebarAntd.scss';
 import { CloseOutlined } from "@ant-design/icons";
 
 type Sidebar_Props = {
@@ -17,13 +17,13 @@ const Sidebar_Mobile: React.FC<Sidebar_Props> = ({ collapsed, onCollapse, items 
 
     <>
       <Sider
-        width={360} // Chiều rộng Sider khi mở ra
+        width={320} // Chiều rộng Sider khi mở ra
         collapsedWidth={collapsed ? 0 : 100} // Đặt collapsedWidth để rộng hơn một chút trên mobile
         breakpoint="lg"
         collapsed={collapsed}
         onCollapse={(isCollapsed) => onCollapse(isCollapsed)}
         style={{ minWidth: '360px', maxWidth: '360px', width: '360px' }}
-        className="sidebarMobile block sm:block lg:hidden min-h-screen fixed z-50 "
+        className="sidebar block sm:block lg:hidden min-h-screen fixed z-50 "
         theme="dark"
         trigger={null}
       >
