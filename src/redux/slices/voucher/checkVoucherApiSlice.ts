d4f8cbde97a9d8339e49c2749a75401fe8a5ed86
vng004 <giaothot2004@gmail.com>
2024-11-22 12:55:30 +0700

    #UME-246 feat:Call api để cho người dùng thanh toán được khóa học thông qua VNPAY

---
+++ src/redux/slices/voucher/checkVoucherApiSlice.ts
@@ -0,0 +1,22 @@
+import { baseUrl } from '@/Api'
+import { TVoucher } from '@/interfaces/TVoucher'
+import { createApi } from '@reduxjs/toolkit/query/react'
+
+export const checkVoucherApiSlice = createApi({
+  reducerPath: 'voucherApi',
+  baseQuery: baseUrl,
+  tagTypes: ['Voucher'],
+  endpoints: (builder) => ({
+    checkVoucher: builder.mutation<TVoucher, TVoucher>({
+      query: (body) => ({
+        url: '/vouchers/check',
+        method: 'POST',
+        body
+      }),
+      invalidatesTags:['Voucher'],
+      transformResponse: (res : { data: TVoucher }) => res.data
+    })
+  })
+})
+
+export const {useCheckVoucherMutation} = checkVoucherApiSlice
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,13 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './rootReducer'
-import { courseSlice } from "./slices/courseSlice";
-import { profileApiSlice } from './slices/profile/profileApiSlice';
-import { authApiSlice } from './slices/auth/authApiSlice';
+import { courseSlice } from './slices/courseSlice'
+import { profileApiSlice } from './slices/profile/profileApiSlice'
+import { authApiSlice } from './slices/auth/authApiSlice'
+import { checkOutApiSlice } from './slices/payment/checkoutApiSlice'
+import { checkVoucherApiSlice } from './slices/voucher/checkVoucherApiSlice'
 
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(authApiSlice.middleware, profileApiSlice.middleware, courseSlice.middleware)
+    getDefaultMiddleware().concat(
+      authApiSlice.middleware,
+      profileApiSlice.middleware,
+      courseSlice.middleware,
+      checkOutApiSlice.middleware,
+      checkVoucherApiSlice.middleware
+    )
 })
 
 export type RootState = ReturnType<typeof store.getState>
